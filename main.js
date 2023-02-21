a168f237ea1e03166118aa116987f9baecb84cc8
bm <121493355+b-mat7@users.noreply.github.com>
2023-02-21 22:55:59 +0100

    Steps 6 & 7, minor refactoring

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const mockUpStrand = () => {
 
 
 // Mutates a random DNA base
-const mutateRandBase = (base) => {
+const mutateRandBase = base => {
     let mutatedBase = base;
     const dnaBases = ['A', 'T', 'C', 'G'];
     do {
@@ -35,11 +35,15 @@ const pAequorFactory = (num, dnaArray) => {
     return {
         specimenNum : num,
         dna : dnaArray,
+        
+        // changes random DNA base into a different base
         mutate(){
             let i = Math.floor(Math.random() * 15);
             this.dna[i] = mutateRandBase(this.dna[i]);
             return this.dna;    
         },
+        
+        // compares each base and returns match %
         compareDNA(pAequor){
             let hits = 0;
             for(let i = 0 ; i < this.dna.length ; i++){
@@ -48,13 +52,21 @@ const pAequorFactory = (num, dnaArray) => {
                 }; 
             };
             console.log(`Specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${roundTo((hits/this.dna.length) * 100, 2) }% DNA in common`);
+        },
+
+        // returns true only of 60% of bases are 'C' or 'G'
+        willLikelySurvive(){
+            const cgItems = this.dna.filter(item => item === 'C' || item === 'G');
+            console.log(...cgItems);
+            console.log(cgItems.length / this.dna.length);
+            return (cgItems.length / this.dna.length >= 0.6) ? true : false;
         }
     }
 };
 
 
 // create sample objects from pAequorFactory
-const makeMeSamples = (num) => {
+const makeSamples = num => {
     const sampleObjects = [];
     for(let i=1 ; i <= num ; i++){
         sampleObjects.push(pAequorFactory(i, mockUpStrand()));
@@ -63,8 +75,21 @@ const makeMeSamples = (num) => {
 };
 
 
-let samples = makeMeSamples(2);
+// create surviveable sample objects from pAequorFactory
+const makeSurviveableSamples = num => {
+    const sampleObjects = [];
+    let i = 1;
+    do {
+        let testObj = pAequorFactory(i, mockUpStrand());
+        testObj.willLikelySurvive() === true ? sampleObjects.push(testObj) : false;
+        i ++;
+    } while(sampleObjects.length < num);
+    return sampleObjects;
+};
+
+
+
+let samples = makeSurviveableSamples(30);
 console.log(...samples);
-samples[0].compareDNA(samples[1]);
-  
-  
+// samples[0].compareDNA(samples[1]);
+// console.log(samples[0].willLikelySurvive());  
